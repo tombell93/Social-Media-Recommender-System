f648d1cbca1b1cae0e43457903a9eb281f850405
Tom Bell <tom@tom-Gaming-PC.(none)>
2013-10-09 13:08:55 +0100

    Fixed problem

--- v1.0.0/src/Prioritiser.java
+++ v1.0.0/src/Prioritiser.java
@@ -20,5 +20,27 @@ public class Prioritiser {
         // TODO code application logic here
         System.out.println("This is a ranking agent.");
         Gson gson = new GsonBuilder().serializeNulls().create();
+        
+         /* 
++         * 1. Load test data into list of raw data objects
++         * 2. Parse them one by one into:
++         *      a. Parse into DataItem object
++         *          i. Parse data fields from raw data object into DataItem
++         *          ii. Perform topic analysis
++         *          iii. Store topic data in Relevance object
++         *      b. Persist ContextRelevance object
++         *          i. Includes:
++         *              - Time of day
++         *              - Matrix describing users topcic priorities 
++         *              - Matrix describing learned data about users priorities
++         *      c. Perform ranking using good ranking matrix-based algorithm
++         *          i. Give each DataItem a score 
++         *              - Based upon how well Relevance and ContextRelevance
++         *                  objects match
++         *          ii. Maintain unsorted DataItems in List<DataItem>
++         *      d. Place new DataItem into list according to score
++         */
+
+        
     }
 }
